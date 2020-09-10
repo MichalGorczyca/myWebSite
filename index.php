8ba4fd2c718f7c7c06cdfce2268e7d3b8816ae17
Michal Gorczyca <michal.gorc@gmail.com>
2020-09-10 14:08:40 +0200

    Adding links to pages

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website by Michał Gorczyca</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <script src="https://kit.fontawesome.com/7e02ae03b6.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <div class="container">
+        <div class="bow"></div>
+        <div class="box">
+            <div class="image"><img src="images/profilowe.jpg" width="100" alt=""></div>
+            <div class="me">
+                <p>Michał</p>
+                <p class="lastName">Gorczyca</p>
+            </div>
+        </div>
+
+        <div class="text">
+            <p>Hi! Welcome to Home Page</p>
+        </div>
+        <button class="btn"><a class="text-white text-decoration-none" href="mainContent.php">Start</a></button>
+
+        
+
+        <div class="icons ">
+            <a href="#"><i class="fab fa-facebook px-3"></i></a>
+            <a href="#"><i class="far fa-envelope px-3"></i></a>
+            <a href="#"><i class="fab fa-github pl-3"></i></a>
+        </div>
+    </div>
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</html>
--- mainContent.php
+++ mainContent.php
@@ -19,25 +19,25 @@
             <div class="text"> 
                 I am glad to see <b>you</b> on my website! I am begginer <b>programer and web developer</b> who wants to <b>challange</b> himself by creating something on his own. By far I was creating websites only on Udemy courses. I am spending so much time on this platform due to vacation and pandemic time so I can be better and better in programing.
             </div>
-            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center ">Click here to see my projects</h4>
+            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center "><a href="#" class="text-white text-decoration-none">Click here to see my projects</a></h4>
             </div>    
         </div>
         <div class="content-right">
             <div class="text"> My plan is to gain knowledge by experiance so I created blog. You can find there many topics I am intersted in and give your opinion by getting in comment section. You can also create your own topics so I can know you better ^^. I am looking for interesting conversation. 
             </div>
-            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center ">Go to my blog</h4>
+            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center "><a href="#" class="text-white text-decoration-none">Go to my blog</a></h4>
             </div>    
         </div>
         <div class="content-left">
             <div class="text">Are you interested in what exacly is my experiance or who I am? Maybe you want to know where I come from and what schools I graduated or just you are curiuos what kind of person am I and want to know me better ^^. All the informations you can find in About Me section. Go check it right now!
             </div>
-            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center ">Go to About Me</h4>
+            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center "><a class="text-white text-decoration-none" href="/myWebSite/aboutme.php">Go to About Me</a></h4>
             </div>    
         </div>
         <div class="content-right">
             <div class="text"> Last but not least: Conatacting Page. You can go there if you admire my work and want to hire me for your project. Please contact me via this page, I promise I will reply as quick as possible.
             </div>
-            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center ">Contact Me</h4>
+            <div class="projectbutton"><h4 class="font-weight-bold text-white text-center "><a class="text-white text-decoration-none" href="">Contact Me</a></h4>
             </div>    
         </div>
         
