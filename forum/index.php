8a8f63e74d3b6e3dedd10a09ca54f12d64796ba7
Michal Gorczyca <michal.gorc@gmail.com>
2020-09-15 16:25:46 +0200

    Adding bootstrap to forum

---
+++ forum/index.php
@@ -0,0 +1,78 @@
+<?php include "includes/forumHeader.php"; ?>
+
+    <?php include "includes/forumNav.php"; ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main mt-3 mx-5 p-3">
+                <div class="h3 text-left float-left text-uppercase">Welcome on forum</div> 
+                <div class="py-2 h6 text-right"><span class="px-2">Login</span> <span>Register</span></div>
+                
+            </div>
+            <div class="box container-fluid mt-3 mx-5">
+                <div class="row">
+                    <div class="content col-8 p-0">
+                        <div class="important">
+                            <div class="title p-2 container-fluid">Important</div>
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Author</th>
+                                        <th scope="col">Topic</th>
+                                        <th scope="col">Views</th>
+                                        <th scope="col">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td scope="row">Michał Gorczyca</td>
+                                        <td>Important Topic</td>
+                                        <td>4</td>
+                                        <td>15.09.2020</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="forums mt-3">
+                            <div class="title p-2 container-fluid">Forums</div>
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Author</th>
+                                        <th scope="col">Topic</th>
+                                        <th scope="col">Views</th>
+                                        <th scope="col">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td scope="row">Michał Gorczyca</td>
+                                        <td>Topic</td>
+                                        <td>4</td>
+                                        <td>15.09.2020</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">Michał Gorczyca</td>
+                                        <td>Topic</td>
+                                        <td>4</td>
+                                        <td>15.09.2020</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">Michał Gorczyca</td>
+                                        <td>Topic</td>
+                                        <td>4</td>
+                                        <td>15.09.2020</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="sidebar col-3 ml-auto">
+                        <div class="title p-2 container-fluid">Sidebar</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include "includes/forumFooter.php"; ?>
