a5ce5e5588951caaf3fa75fe25ab8b6a8246a7dc
Michal Gorczyca <michal.gorc@gmail.com>
2020-09-19 21:06:00 +0200

    Home Page changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website by Michał Gorczyca</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/Style.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <script src="https://kit.fontawesome.com/7e02ae03b6.js" crossorigin="anonymous"></script>
 </head>
 <body style="background-image: url('images/background.jpg')">
